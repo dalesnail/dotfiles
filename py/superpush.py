5758a2c77c66d664a0a8cf47fc52cd741b8c1d3b
dalesnail <cameronmills.work@gmail.com>
2018-03-31 18:30:32 -0400

    SuperPush Update

--- py/superpush.py
+++ py/superpush.py
@@ -4,7 +4,6 @@ from subprocess import call
 import os
 
 os.chdir("/home/dalesnail/.dotfiles")
-#call('cd ~/.dotfiles', shell = True)
 call('git add .', shell = True)
 call('git commit -m "SuperPush Update"', shell = True)
 call('git push origin master', shell = True)
